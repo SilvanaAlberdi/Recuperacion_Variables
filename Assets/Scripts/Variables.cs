890f51002decd9ff3ce1a547a9774e90bdfe6f94
SilvanaAlberdi <silvanaalberdi@ni.eus>
2023-05-09 10:46:20 +0200

    First four excercises

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    public int points;
+    private float speed = 1.5f;
+    [SerializeField] private bool isGameOver;
+    public string welcomeMessage = "¡Bienvenido!";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
